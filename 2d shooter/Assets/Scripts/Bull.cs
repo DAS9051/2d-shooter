de2a457c186c66163a0b3b1d6f562f0ee2426ae6
DAS9051 <dylan.a.sahota@gmail.com>
2024-01-13 18:42:43 -0500

    added player that can shoot

---
+++ 2d shooter/Assets/Scripts/Bull.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bull : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [Range(1, 10)]
+    [SerializeField] private float speed = 10f;
+
+    [Range(1, 10)]
+    [SerializeField] private float lifetime = 3f;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, lifetime);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = transform.up * speed;
+    }
+
+
+
+}
--- 2d shooter/Assets/Scripts/Player.cs
+++ 2d shooter/Assets/Scripts/Player.cs
@@ -7,9 +7,17 @@ public class Player : MonoBehaviour
     [SerializeField] private float speed = 5f;
     // Start is called before the first frame update
 
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firingPoint;
+    [Range(0.1f, 2f)]
+    [SerializeField] private float fireRate = 0.5f;
+
+
     private Rigidbody2D rb;
     private float mx;
     private float my;
+
+    private float fireTimer;
 
     private Vector2 mousePos;
 
@@ -28,11 +36,28 @@ public class Player : MonoBehaviour
         float angle = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x) * Mathf.Rad2Deg - 90f;
 
         transform.localRotation = Quaternion.Euler(0, 0, angle);
+
+        if (Input.GetMouseButton(0) && fireTimer <= 0f)
+        {
+            Shoot();
+            fireTimer = fireRate;
+        }
+        else
+        {
+            fireTimer -= Time.deltaTime;
+        }
     }
 
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(mx, my).normalized * speed;
+    }
+
+
+    private void Shoot()
+    {
+        Instantiate(bulletPrefab, firingPoint.position, firingPoint.rotation);
+        fireTimer = 0;
     }
 }
